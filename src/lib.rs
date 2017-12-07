e478f0a163011f1ef6bf6ede6013b466d8a0d2e4
Ivan Mironov <mironov.ivan@gmail.com>
2017-12-07 10:52:35 +0300

    Create new crate.

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(feature = "unstable", warn(unreachable_pub))]
+#![forbid(unsafe_code)]
+#![warn(unused_results)]
+
+#![cfg_attr(feature = "cargo-clippy", warn(filter_map))]
+#![cfg_attr(feature = "cargo-clippy", warn(if_not_else))]
+#![cfg_attr(feature = "cargo-clippy", warn(mut_mut))]
+#![cfg_attr(feature = "cargo-clippy", warn(non_ascii_literal))]
+#![cfg_attr(feature = "cargo-clippy", warn(option_map_unwrap_or))]
+#![cfg_attr(feature = "cargo-clippy", warn(option_map_unwrap_or_else))]
+#![cfg_attr(feature = "cargo-clippy", warn(single_match_else))]
+#![cfg_attr(feature = "cargo-clippy", warn(wrong_pub_self_convention))]
+#![cfg_attr(feature = "cargo-clippy", warn(use_self))]
+#![cfg_attr(feature = "cargo-clippy", warn(used_underscore_binding))]
+#![cfg_attr(feature = "cargo-clippy", warn(print_stdout))]
+
+extern crate futures;
